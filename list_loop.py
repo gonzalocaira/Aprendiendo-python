11b90527f4f23fe8f0e96a831d3b9a2d97adeeaa
Gonzalo Caira <gc98wootz@gmail.com>
2018-12-29 19:46:48 -0500

    Loops and list

---
+++ list_loop.py
@@ -0,0 +1,53 @@
+
+magicians = ['alice','david','carolina']
+
+for magician in magicians:
+    print(magician)
+
+list_new = ['111111','5','6','9','2']
+print(list_new)
+list_num= [9,8,3,7,56,63]
+print(list_num)
+list_new.sort()
+print(list_new)
+list_num.sort(reverse=1)
+list_num.insert(2,0)
+list_num.append(4)
+print(list_num)
+
+for num in magicians:
+    print(num + " that")
+    print("was a great trick!"+".\n")
+    #print('2\n')
+
+    print ("xd")
+    
+##WTF!!!
+magic = ['alice', 'david', 'carolina']
+for magi in magic:
+    print(magi.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+    print("Thank you everyone, that was a great magic show!")
+# does not print last value
+for value in range(1,6):
+    print(value)
+
+#using range () to make a list of numbers
+
+numbers = list(range(10,16))
+print(numbers)
+
+#more
+
+even_numbers = list(range(2,11,2))
+print(even_numbers)
+
+#playing 
+# ; ; ;
+squares = []
+for value in range (1,11):
+    square = value**2;
+    squares.append(square);
+
+print(squares)
